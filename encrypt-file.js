50f623dca0bc66f710c9e4db73b0383b0906f8d4
Anthony Metzidis <anthony.metzidis@gmail.com>
2018-12-22 22:48:15 +0000

    tar and encrypt

--- encrypt-file.js
+++ encrypt-file.js
@@ -1,22 +1,47 @@
 var fs = require('fs');
+var tar = require('tar')
 var openpgp = require('openpgp')
-var readStream = fs.createReadStream('tmp/infile.tar');
+
 var writeStream = fs.createWriteStream('tmp/outfile.bin')
 
-const options = {
-    message: openpgp.message.fromBinary(readStream), // input as Message object
-    passwords: ['secret stuff'],                         // multiple passwords possible
-    armor: false                                         // don't ASCII armor (for Uint8Array output)
-};
 
-openpgp.encrypt(options).then(async function(ciphertext) {
-    const encrypted = ciphertext.message.packets.write(); // get raw encrypted packets as ReadableStream<Uint8Array>
-     console.log(encrypted)
-     const reader = openpgp.stream.getReader(encrypted);
-    while (true) {
-        const { done, value } = await reader.read();
-        if (done) break;
-        writeStream.write(value)
-    }
-    console.log("writing a block", ciphertext.message.packets.length)
-});
+tar.c({
+        gzip: false,
+        file: 'tmp/infile.tar'
+    }, ['tmp/testtar'])
+    .then(console.log)
+    .then(() => {
+        
+        const options = {
+            message: openpgp.message.fromBinary( fs.createReadStream('tmp/infile.tar')), // input as Message object
+            passwords: ['secret stuff'], // multiple passwords possible
+            armor: false // don't ASCII armor (for Uint8Array output)
+        };
+
+        openpgp.encrypt(options).then(async function(ciphertext) {
+            const encrypted = ciphertext.message.packets.write(); // get raw encrypted packets as ReadableStream<Uint8Array>
+            console.log(encrypted)
+            const reader = openpgp.stream.getReader(encrypted);
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) break;
+                writeStream.write(value)
+            }
+            console.log("writing a block", ciphertext.message.packets.length)
+        });
+
+       
+
+        openpgp.encrypt(options).then(async function(ciphertext) {
+            const encrypted = ciphertext.message.packets.write(); // get raw encrypted packets as ReadableStream<Uint8Array>
+            console.log(encrypted)
+            const reader = openpgp.stream.getReader(encrypted);
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) break;
+                writeStream.write(value)
+            }
+            console.log("writing a block", ciphertext.message.packets.length)
+        });
+
+    });
